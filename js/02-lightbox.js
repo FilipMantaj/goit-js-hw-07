97237d617a424dd07eda7f79960f3e85f982553b
FilipMantaj <145697524+FilipMantaj@users.noreply.github.com>
2024-01-29 21:50:28 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt="${description}" />
+      </a></li>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", listItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
 console.log(galleryItems);
